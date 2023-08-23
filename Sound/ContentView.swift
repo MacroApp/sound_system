be021469de794023a74bab0a9e6a91d7a646c3cf
AIWASS23 <marcelodearaujo677@gmail.com>
2023-08-23 08:52:10 -0300

    POC's sobre sons do sistema 🔊

--- Sound/ContentView.swift
+++ Sound/ContentView.swift
@@ -6,16 +6,31 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct ContentView: View {
+
+    let systemSounds:[String] = ["1000","1001","1002","1003","1004","1005","1006","1008","1009","1010","1013","1014","1016","1020","1021","1022","1023","1024","1025","1026","1027","1028","1029","1030","1031","1032","1033","1034","1035","1036","1050","1051","1052","1053","1054","1057","1070","1072","1073","1074","1075","1100","1103","1104","1105","1106","1107","1108","1109","1110","1111","1112","1113","1114","1115","1116","1118","1150","1152","1153","1154","1200","1201","1202","1203","1204","1205","1206","1207","1208","1209","1210","1211","1254","1255","1256","1258","1259","1300","1301","1302","1303","1304","1305","1306","1307","1308","1309","1310","1311","1313","1314","1315","1320","1321","1322","1323","1324","1325","1326","1327","1328","1329","1330","1331","1332","1333","1334","1335","1336","1350"]
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            ScrollView(.vertical,showsIndicators: false) {
+                VStack(spacing: 10){
+                    ForEach(0..<systemSounds.count) { index in
+                        Button {
+                            if let sound = Int(systemSounds[index]){
+                                AudioServicesPlaySystemSound(SystemSoundID(sound))
+                            }
+                        } label: {
+                            Label("\(systemSounds[index])", systemImage: "speaker.wave.1.fill")
+                        }
+                        .withCustomButtonStyle()
+                    }
+                }
+            }
+            .navigationTitle("System Sounds")
+            .padding(.horizontal)
         }
-        .padding()
     }
 }
 
@@ -24,3 +39,27 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+struct CustomButtonStyle: ButtonStyle{
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .font(.title2)
+            .fontWeight(.bold)
+            .foregroundColor(.white)
+            .frame(maxWidth: .infinity)
+            .frame(height: 80)
+            .background(Color(red: CGFloat.random(in: 0...1), green: CGFloat.random(in: 0...1), blue: CGFloat.random(in: 0...1)).gradient.opacity(1))
+            .cornerRadius(10)
+            .shadow(radius: 10)
+            .scaleEffect(configuration.isPressed ? 0.9 : 1.0)
+    }
+}
+
+
+extension View{
+    func withCustomButtonStyle() -> some View{
+        buttonStyle(CustomButtonStyle())
+    }
+}
+
