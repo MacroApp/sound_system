d18e45e0bfaae059223e16f6fdbee1b74318ba39
AIWASS23 <marcelodearaujo677@gmail.com>
2023-08-23 08:19:49 -0300

    Initial Commit

---
+++ Sound/SoundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundApp.swift
+//  Sound
+//
+//  Created by Marcelo De Araújo on 23/08/23.
+//
+
+import SwiftUI
+
+@main
+struct SoundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
